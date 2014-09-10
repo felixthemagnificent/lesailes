0d9477584cbe12b1b2443687f40532d9c86563d4
Felix <felix@Felix-Mac-mini.local>
2014-09-10 17:20:32 +0500

    Initial Commit

---
+++ lesailes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  lesailes
+//
+//  Created by Felix on 9/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ lesailes/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  lesailes
+//
+//  Created by Felix on 9/10/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
